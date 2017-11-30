576f313c6320f5d0f90abedc44dda7b152f650ca
Tom Wyant <wyant@cpan.org>
2017-11-30 12:32:01 -0500

    Remove mailto: bug reporting metadata.

--- inc/My/Module/Meta.pm
+++ inc/My/Module/Meta.pm
@@ -40,7 +40,7 @@ sub meta_merge {
 	resources	=> {
 	    bugtracker	=> {
                 web	=> 'https://rt.cpan.org/Public/Dist/Display.html?Name=Astro-UTDF',
-                mailto  => 'wyant@cpan.org',
+#                mailto  => 'wyant@cpan.org',
             },
 	    license	=> 'http://dev.perl.org/licenses/',
 	    repository	=> {
