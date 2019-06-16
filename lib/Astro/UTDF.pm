73cfbdacad16abb2ba544c46e05187531443fa88
Tom Wyant <wyant@cpan.org>
2019-06-16 08:59:31 -0400

    Version 0.008
    Set version to 0.008. Update Changes file for changes since v00.007_01.

--- lib/Astro/UTDF.pm
+++ lib/Astro/UTDF.pm
@@ -19,7 +19,7 @@ use constant SPEED_OF_LIGHT => 299792.458;	# Km/sec, per U.S. NIST
 use constant ARRAY_REF	=> ref [];
 use constant CODE_REF	=> ref sub {};
 
-our $VERSION = '0.007_01';
+our $VERSION = '0.008';
 
 sub new {
     my $class = shift;
